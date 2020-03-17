45095a09394c74079231691051035b0b1363ed45
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-17 19:31:38 -0400

    install styled-components

--- team-builder/src/Components/TeamMembers.js
+++ team-builder/src/Components/TeamMembers.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Test = styled.div`
+  backgroundColor: red;
+`;
 
 const TeamMembers = props => {
   console.log(props)
   return (
     <div>
+      <Test />
       <h2>Team Members</h2>
       {props.teamMembers.map(member => (
         <div key={member.id} className='member-card'>
